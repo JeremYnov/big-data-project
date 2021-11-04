81966e232cc182ae57b91442da11932300426fd5
Jérémy <jeremy.richard@ynov.com>
2021-11-04 09:59:59 +0100

    added first version of ccxt script

--- script.py
+++ script.py
@@ -1,26 +1,20 @@
- #This example uses Python 2.7 and the python-request library.
+import ccxt 
+import pandas as pd 
+import time
 
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-import json
+exchange = ccxt.binanceus()
+markets = exchange.load_markets()
+ticker = exchange.fetch_ticker('BTC/USD')
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-parameters = {
-  'start':'1',
-  'limit':'5000',
-  'convert':'USD'
-}
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
-}
+df = pd.DataFrame(markets)
+columns = df.columns
+USDCrypto = []
+for column in columns : 
+    splitedColumn = column.split('/')
+    if splitedColumn[1] == 'USD':
+        print(column)
+        USDCrypto.append(exchange.fetch_ticker(str(column)))
+            
 
-session = Session()
-session.headers.update(headers)
-
-try:
-  response = session.get(url, params=parameters)
-  data = json.loads(response.text)
-  print(data)
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
+dfCrypto = pd.DataFrame(USDCrypto)   
+dfCrypto.head(1)

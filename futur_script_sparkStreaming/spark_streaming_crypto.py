4da26a0a21b837b4756db00ab2f04a738d20a055
Jérémy <jeremy.richard@ynov.com>
2021-11-15 21:43:42 +0100

    added wordcloud

--- futur_script_sparkStreaming/spark_streaming_crypto.py
+++ futur_script_sparkStreaming/spark_streaming_crypto.py
@@ -31,10 +31,9 @@ while True :
             symbol = symbol_db.find_one({"name": crypto["symbol"]}) # recuperation du symbol crypto ex (BTC/USD)
             crypto_db.insert_one({"date_time": crypto["datetime"], "price": crypto["last"], "symbol": symbol["_id"]}) #INSERTION CRYPTO
             ########################################
-            print("symbol")
+            print(symbol)
 
     sleep(5)
-    print("")
 
 
 # RECUPERATION DONNE D'UNE CRYPTO
